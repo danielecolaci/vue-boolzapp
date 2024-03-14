36d44f812b7fa2e289317805084e4374e46a3978
danielecolaci <danielecolaci@outlook.com>
2024-03-14 12:32:11 +0100

    Set data contacts in js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,215 @@
-console.log('It works');
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            contacts: [
+                {
+                    name: 'Michele',
+                    avatar: './assets/img/avatar_1.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/05/2024 15:30:55',
+                            message: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/05/2024 15:50:00',
+                            message: 'Ricordati di stendere i panni',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/05/2024 16:15:22',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Fabio',
+                    avatar: './assets/img/avatar_2.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '20/04/2024 16:30:00',
+                            message: 'Ciao come stai?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '20/04/2024 16:30:55',
+                            message: 'Bene grazie! Stasera ci vediamo?',
+                            status: 'received'
+                        },
+                        {
+                            date: '20/04/2024 16:35:00',
+                            message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                            status: 'sent'
+                        }
+                    ],
+                },
+                {
+                    name: 'Luca',
+                    avatar: './assets/img/avatar_3.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '28/03/2024 10:10:40',
+                            message: 'La Marianna va in campagna',
+                            status: 'received'
+                        },
+                        {
+                            date: '28/03/2024 10:20:10',
+                            message: 'Sicuro di non aver sbagliato chat?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '28/03/2024 16:15:22',
+                            message: 'Ah scusa!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Danilo',
+                    avatar: './assets/img/avatar_4.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '15/02/2024 15:30:55',
+                            message: 'Lo sai che ha aperto una nuova pizzeria?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '15/02/2024 15:50:00',
+                            message: 'Si, ma preferirei andare al cinema',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Davide',
+                    avatar: './assets/img/avatar_5.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '23/01/2024 15:30:55',
+                            message: 'Ricordati di chiamare la nonna',
+                            status: 'sent'
+                        },
+                        {
+                            date: '23/01/2024 15:50:00',
+                            message: 'Va bene, stasera la sento',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Alessandra',
+                    avatar: './assets/img/avatar_6.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '19/01/2024 15:30:55',
+                            message: 'Ciao Claudia, hai novità?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '19/01/2024 15:50:00',
+                            message: 'Non ancora',
+                            status: 'received'
+                        },
+                        {
+                            date: '19/01/2024 15:51:00',
+                            message: 'Nessuna nuova, buona nuova',
+                            status: 'sent'
+                        }
+                    ],
+                },
+                {
+                    name: 'Giovanni',
+                    avatar: './assets/img/avatar_7.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '15/01/2024 15:30:55',
+                            message: 'Fai gli auguri a Federica che è il suo compleanno!',
+                            status: 'sent'
+                        },
+                        {
+                            date: '15/01/2024 15:50:00',
+                            message: 'Grazie per avermelo ricordato, le scrivo subito!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Susanna',
+                    avatar: './assets/img/avatar_8.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2024 15:30:55',
+                            message: 'Ciao, andiamo a mangiare la pizza stasera?',
+                            status: 'received'
+                        },
+                        {
+                            date: '10/01/2024 15:50:00',
+                            message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2024 15:51:00',
+                            message: 'OK!!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Federica',
+                    avatar: './assets/img/avatar_9.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '09/01/2024 15:30:55',
+                            message: 'Buon Compleanno!',
+                            status: 'sent'
+                        },
+                        {
+                            date: '09/01/2024 15:50:00',
+                            message: 'Grazie mille, a presto!',
+                            status: 'received'
+                        },
+                        {
+                            date: '09/01/2024 15:51:00',
+                            message: '<3',
+                            status: 'sent'
+                        }
+                    ],
+                },
+                {
+                    name: 'Tina',
+                    avatar: './assets/img/avatar_10.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '08/01/2024 15:30:55',
+                            message: 'Domani vai a fare la spesa?',
+                            status: 'received'
+                        },
+                        {
+                            date: '08/01/2024 15:50:00',
+                            message: 'Penso di sì, ti faccio sapere',
+                            status: 'sent'
+                        },
+                        {
+                            date: '08/01/2024 15:51:00',
+                            message: 'OK!!',
+                            status: 'received'
+                        }
+                    ],
+                }
+            ]
+        }
+    }
+}).mount('#app')
